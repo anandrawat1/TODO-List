96f06b63cd3e5c049644a447a0bf4f8625820914
anandrawat1 <138303378+anandrawat1@users.noreply.github.com>
2025-08-13 11:11:12 +0530

    hello

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Fragment } from "react";
+
+import {Todo} from "./Todo/Todo.jsx";
+
+export const App = () => {
+  return (
+    <section>
+      <Todo/>
+    </section>
+  )
+}
---
+++ src/Todo/Todo.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import "./todo.css";
+import {TodoForm} from "./TodoForm.jsx";
+import {TodoList} from "./TodoList.jsx";
+import { TodoDate } from "./TodoDate.jsx";
+import {getLocalStorageTodoData,
+     setLocalStorageTodoData} from "./TodoLocalStorage.jsx"
+
+
+export const Todo = () => {
+    
+    const [task, setTask] = useState(() =>getLocalStorageTodoData());
+
+
+    
+
+    const handleFormSubmit = (inputValue) => {
+        const{ id , content, checked} = inputValue;
+       
+        if(!content) return;
+        //  if(task.includes(inputValue)) return
+        const ifTodoContentMatched = task.find(
+             
+            (curTask) => curTask.content == content
+        );
+        if(ifTodoContentMatched) return;
+        setTask((prevTask) => [...prevTask, inputValue]);
+        
+    };
+        setLocalStorageTodoData(task);
+
+
+    // handleDeleteTodo 
+    const handleDeleteTodo = (value) => {
+        const updatedTask = task.filter((curTask) => curTask.content !== value);
+        setTask(updatedTask);
+    }
+
+
+    // handleclear todo data
+    const handleClearTodoData = () => {
+        setTask([]);
+    }
+    const handleCheckedTodo = (content) => {
+        const updatedTask = task.map((curTask) =>{
+            if(curTask.content === content){
+                return {...curTask, checked: !curTask.checked};
+
+            }
+            else {
+                return curTask;
+            }
+        });
+        setTask(updatedTask)
+    }
+
+
+    
+    return (
+        <section className="todo-container">
+            <header>
+                <h1>Todo List</h1>
+                <TodoDate/>
+            </header>
+            
+            <TodoForm onAddTodo = {handleFormSubmit}/>
+            <section className="myUnOrdList">
+                <ul>
+                    {
+                        task.map((curTask) =>{
+                            return <TodoList
+                            key={curTask.id}
+                            data = {curTask.content}
+                            checked = {curTask.checked}
+                            onHandleDeleteTodo = {handleDeleteTodo}
+                            onHandleCheckedTodo = {handleCheckedTodo}
+                            />
+                        })
+                    }
+                </ul>
+
+            </section>
+            <section>
+                <button className="clear-btn" onClick={handleClearTodoData}>Clear All </button>
+                
+            </section>
+        </section>
+    )
+}

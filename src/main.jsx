8d78733eed0289dccf671960576c86cacd9584bc
anandrawat1 <138303378+anandrawat1@users.noreply.github.com>
2025-08-18 22:46:24 +0530

    edited json file

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import './index.css'
-import App from './App.jsx'
-import { Todo } from './Todo/Todo.jsx'
+import {App} from './App.jsx'
+// import { Todo } from './Todo/Todo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <Todo/>
+    <App />
+    {/* <Todo/> */}
   </StrictMode>,
 )
